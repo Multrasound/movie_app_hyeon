d53ebfbca92376d1dd69c854687d399b7357ce60
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-07-16 23:23:49 +0900

    Foot Component 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+function Food({fav}) {
+  return <h1>I love {fav}</h1>
+}
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <Food fav={"kimchi"}/>
+    </div>
+  )
 }
 
 export default App;
+ 
