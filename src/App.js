b05b51aa923d632e0224e2d91303d6fe16aa5cac
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-07-22 22:32:06 +0900

    map 함수 적용, prop-types 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({fav}) {
-  return <h1>I love {fav}</h1>
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  stars: PropTypes.number.isRequired
+};
+
+function Food({name, stars}) {
+  return <h1>I love {name} ({stars})</h1>
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    stars: 3
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    stars: 5
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food fav={"kimchi"}/>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} stars={dish.stars} />
+      ))}
     </div>
   )
 }
